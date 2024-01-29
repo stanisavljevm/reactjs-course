ac180b1efec541649254e5a78d5e1976a77add4c
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-01-29 16:55:28 +0100

    javascript logic in components

--- 01-pizza-menu/src/index.js
+++ 01-pizza-menu/src/index.js
@@ -74,6 +74,12 @@ function Menu() {
 }
 
 function Footer() {
+	const hour = new Date().getHours();
+	const openHour = 12;
+	const closeHour = 22;
+
+	const isOpen = hour >= openHour && hour <= closeHour;
+	console.log(isOpen);
 	return <footer>We're currently open!</footer>;
 }
 
