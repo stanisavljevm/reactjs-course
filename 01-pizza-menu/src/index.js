28aa23e962e58470ecf2012eb29af23acf083ae3
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-01-29 20:33:57 +0100

    conditional rendering with &&

--- 01-pizza-menu/src/index.js
+++ 01-pizza-menu/src/index.js
@@ -69,14 +69,19 @@ function Header() {
 }
 
 function Menu() {
+	const pizzas = pizzaData;
+
+	const numPizzas = pizzas.length;
 	return (
 		<main className="menu">
 			<h2>Our menu</h2>
-			<ul className="pizzas">
-				{pizzaData.map((pizza) => (
-					<Pizza pizza={pizza} key={pizza.name} />
-				))}
-			</ul>
+			{numPizzas > 0 && (
+				<ul className="pizzas">
+					{pizzas.map((pizza) => (
+						<Pizza pizza={pizza} key={pizza.name} />
+					))}
+				</ul>
+			)}
 		</main>
 	);
 }
@@ -101,7 +106,16 @@ function Footer() {
 
 	const isOpen = hour >= openHour && hour <= closeHour;
 	console.log(isOpen);
-	return <footer className="footer">We're currently open!</footer>;
+	return (
+		<footer className="footer">
+			{isOpen && (
+				<div className="order">
+					<p>We're open until {closeHour}:00. Come visit us or order online.</p>
+					<button className="btn">Order</button>
+				</div>
+			)}
+		</footer>
+	);
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
