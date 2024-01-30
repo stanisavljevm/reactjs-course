a7fb3224abc4c11e2ba3090d11aceefa2179a6a8
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-01-30 16:14:03 +0100

    deleting an item

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -7,11 +7,15 @@ export default function App() {
 		setItems((items) => [...items, item]);
 	}
 
+	function handleDeleteItem(id) {
+		setItems((items) => items.filter((item) => item.id !== id));
+	}
+
 	return (
 		<div className="app">
 			<Logo />
 			<Form onAddItems={handleAddItems} />
-			<PackingList items={items} />
+			<PackingList items={items} onDeleteItem={handleDeleteItem} />
 			<Stats />
 		</div>
 	);
@@ -57,13 +61,13 @@ function Form({ onAddItems }) {
 	);
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
 	return (
 		<>
 			<div className="list">
 				<ul>
 					{items.map((item) => (
-						<Item item={item} key={item.id} />
+						<Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
 					))}
 				</ul>
 			</div>
@@ -72,13 +76,13 @@ function PackingList({ items }) {
 	);
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
 	return (
 		<li>
 			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
 				{item.quantity} {item.description}{" "}
 			</span>
-			<button>❌</button>
+			<button onClick={() => onDeleteItem(item.id)}>❌</button>
 		</li>
 	);
 }
