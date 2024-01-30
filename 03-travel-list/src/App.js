8ab75ed18dde940e26fa975050da0265a6634d8c
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-01-30 11:10:23 +0100

    rendering the items list

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
 	return (
 		<div className="app">
@@ -22,7 +27,29 @@ function Form() {
 }
 
 function PackingList() {
-	return <div className="list">list</div>;
+	return (
+		<>
+			<div className="list">
+				<ul>
+					{initialItems.map((item) => (
+						<Item item={item} />
+					))}
+				</ul>
+			</div>
+			<div></div>
+		</>
+	);
+}
+
+function Item({ item }) {
+	return (
+		<li>
+			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
+				{item.quantity} {item.description}{" "}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 
 function Stats() {
