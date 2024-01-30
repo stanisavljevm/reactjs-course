86c880bd6cafefff3dd0687734bdf3c2d3f23856
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-01-30 11:23:16 +0100

    building a form and handling submissions

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -19,9 +19,22 @@ function Logo() {
 }
 
 function Form() {
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
+
 	return (
-		<form className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your trip?</h3>
+			<select>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input type="text" placeholder="Item..." />
+			<button>Add</button>
 		</form>
 	);
 }
@@ -32,7 +45,7 @@ function PackingList() {
 			<div className="list">
 				<ul>
 					{initialItems.map((item) => (
-						<Item item={item} />
+						<Item item={item} key={item.id} />
 					))}
 				</ul>
 			</div>
