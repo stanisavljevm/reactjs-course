f526fb1de5a5be2db9a632dadf32304b6f7f51b1
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-01-31 12:28:36 +0100

    clearing the list

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -19,6 +19,13 @@ export default function App() {
 		);
 	}
 
+	function handleClearList() {
+		const confirmed = window.confirm(
+			"Are you sure you want to delete all items?"
+		);
+		if (confirmed) setItems([]);
+	}
+
 	return (
 		<div className="app">
 			<Logo />
@@ -27,6 +34,7 @@ export default function App() {
 				items={items}
 				onDeleteItem={handleDeleteItem}
 				onToggleItem={handleToggleItem}
+				onClearList={handleClearList}
 			/>
 			<Stats items={items} />
 		</div>
@@ -73,7 +81,7 @@ function Form({ onAddItems }) {
 	);
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
 	const [sortBy, setSortBy] = useState("input");
 
 	let sortedItems;
@@ -107,6 +115,8 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
 						<option value="description">Sort by description</option>
 						<option value="packed">Sort by packed status</option>
 					</select>
+
+					<button onClick={onClearList}>Clear List</button>
 				</div>
 			</div>
 		</>
