dd35544cac57975b524882985d11335778c620d1
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-02-01 11:35:07 +0100

    creating controlled elements

--- 04-eat-n-split/src/App.js
+++ 04-eat-n-split/src/App.js
@@ -148,19 +148,36 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+	const [bill, setBill] = useState("");
+	const [paidByUser, setPaidByUser] = useState("");
+	const paidByFriend = bill ? bill - paidByUser : "";
+	const [whoIsPaying, setWhoIsPaying] = useState("user");
 	return (
 		<form className="form-split-bill">
 			<h2>Split a bill with {selectedFriend.name}</h2>
 
 			<label>💰 Bill value</label>
-			<input type="text" />
+			<input
+				type="text"
+				value={bill}
+				onChange={(e) => setBill(+e.target.value)}
+			/>
 			<label> 🧍‍♂️ Your expense</label>
-			<input type="text" />
+			<input
+				type="text"
+				value={paidByUser}
+				onChange={(e) =>
+					setPaidByUser(+e.target.value > bill ? paidByUser : +e.target.value)
+				}
+			/>
 			<label> 👫 {selectedFriend.name} 's expense</label>
-			<input type="text" value={selectedFriend.balance} disabled />
+			<input type="text" value={paidByFriend} disabled />
 
 			<label>🤑 Who is paying the bill?</label>
-			<select>
+			<select
+				value={whoIsPaying}
+				onChange={(e) => setWhoIsPaying(e.target.value)}
+			>
 				<option value="user">You</option>
 				<option value="friend">Friend</option>
 			</select>
