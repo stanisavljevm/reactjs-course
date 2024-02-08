daf9b52b24eafd7d1f95e741af123e34693ccd46
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-02-08 18:02:04 +0100

    cleaning up data fetching

--- 01-usepopcorn/src/App.js
+++ 01-usepopcorn/src/App.js
@@ -78,12 +78,15 @@ export default function App() {
 
 	useEffect(
 		function () {
+			const controller = new AbortController();
+
 			async function fetchMovies() {
 				try {
 					setIsLoading(true);
 					setError("");
 					const res = await fetch(
-						`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+						`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+						{ signal: controller.signal }
 					);
 
 					if (!res.ok)
@@ -92,8 +95,11 @@ export default function App() {
 					const data = await res.json();
 					if (data.Response === "False") throw new Error(data.Error);
 					setMovies(data.Search);
+					setError("");
 				} catch (err) {
-					setError(err.message);
+					if (err.name !== "AbortError") {
+						setError(err.message);
+					}
 				} finally {
 					setIsLoading(false);
 				}
@@ -106,6 +112,10 @@ export default function App() {
 			}
 
 			fetchMovies();
+
+			return function () {
+				controller.abort();
+			};
 		},
 		[query]
 	);
