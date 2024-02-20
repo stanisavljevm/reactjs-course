794715a1a4d77eafbd8cd0bfc52a29f784cdddb2
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-02-20 17:33:42 +0100

    creating useKey

--- 01-usepopcorn/src/App.js
+++ 01-usepopcorn/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 import { useMovies } from "./useMovies";
 import { useLocalStorageState } from "./useLocalStorageState";
+import { useKey } from "./useKey";
 
 const KEY = "24766eed";
 
@@ -98,23 +99,12 @@ function Logo() {
 }
 function Search({ query, setQuery }) {
 	const inputEl = useRef(null);
+	useKey("Enter", function () {
+		if (document.activeElement === inputEl.current) return;
+		inputEl.current.focus();
+		setQuery("");
+	});
 
-	useEffect(
-		function () {
-			function callback(e) {
-				if (document.activeElement === inputEl.current) return;
-
-				if (e.code === "Enter") {
-					inputEl.current.focus();
-					setQuery("");
-				}
-			}
-
-			document.addEventListener("keydown", callback);
-			return () => document.addEventListener("keydown", callback);
-		},
-		[setQuery]
-	);
 	return (
 		<input
 			className="search"
@@ -226,20 +216,7 @@ function MovieDetails({ selectedId, watched, onCloseMovie, onAddWatched }) {
 		onCloseMovie();
 	}
 
-	useEffect(
-		function () {
-			function callback(e) {
-				if (e.code === "Escape") onCloseMovie();
-			}
-
-			document.addEventListener("keydown", callback);
-
-			return function () {
-				document.removeEventListener("keydown", callback);
-			};
-		},
-		[onCloseMovie]
-	);
+	useKey("Escape", onCloseMovie);
 
 	useEffect(
 		function () {
