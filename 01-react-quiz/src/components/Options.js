d7f5c02c2028d29cc9d6fb28cac87f44f4e8aaa7
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-02-22 15:48:47 +0100

    handling new answers

--- 01-react-quiz/src/components/Options.js
+++ 01-react-quiz/src/components/Options.js
@@ -1,8 +1,20 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+	const hasAnswered = answer !== null;
 	return (
 		<div className="options">
-			{question.options.map((option) => (
-				<button key={option} className="btn btn-option">
+			{question.options.map((option, i) => (
+				<button
+					key={option}
+					onClick={() => dispatch({ type: "newAnswer", payload: i })}
+					className={`btn btn-option ${i === answer ? "answer" : ""} ${
+						hasAnswered
+							? i === question.correctOption
+								? "correct"
+								: "wrong"
+							: ""
+					}`}
+					disabled={hasAnswered}
+				>
 					{option}
 				</button>
 			))}
