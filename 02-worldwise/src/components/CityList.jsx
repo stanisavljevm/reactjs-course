32053184d0791f9a067046809ec8ce6532834b85
Milorad Stanisavljev <stanisavljevm@outlook.com>
2024-03-12 20:21:01 +0100

    implementing cities list

---
+++ 02-worldwise/src/components/CityList.jsx
@@ -0,0 +1,24 @@
+import Message from "./Message";
+import CityItem from "./CityItem";
+import Spinner from "./Spinner";
+import styles from "./CityList.module.css";
+
+function CityList({ cities, isLoading }) {
+	if (isLoading) return <Spinner />;
+
+	if (!cities.length) {
+		return (
+			<Message message="Add your city by clicking on a city on the map." />
+		);
+	}
+
+	return (
+		<ul className={styles.cityList}>
+			{cities.map((city) => (
+				<CityItem city={city} key={city.id} />
+			))}
+		</ul>
+	);
+}
+
+export default CityList;
